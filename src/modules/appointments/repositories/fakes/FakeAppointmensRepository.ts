6e619ce72320e2a8876056948f358cf0da8c9923
rodrigosakamoto <rodosakamoto@gmail.com>
2020-07-13 08:44:45 -0300

    feat: add create appointments test

--- src/modules/appointments/repositories/fakes/FakeAppointmensRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmensRepository.ts
@@ -1,3 +1,5 @@
+import { uuid } from 'uuidv4';
+
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
@@ -6,13 +8,25 @@ import Appointment from '../../infra/typeorm/entities/Appointment';
 class AppointmentsRepository implements IAppointmentRepository {
   private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {}
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
 
   public async create({
     provider_id,
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
   }
 }
 
