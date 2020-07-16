f0340b65f719c6dfaedaa33dce0f268b6b35304b
rodrigosakamoto <rodosakamoto@gmail.com>
2020-07-16 18:08:28 -0300

    feat: create Appointments

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,11 +15,23 @@ describe('ListProviderDayAvailablity', () => {
   it('should be able to list the day availability from provider ', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 6, 20, 8, 0, 0),
+      user_id: 'user',
+      date: new Date(2020, 6, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 6, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 6, 20, 11).getTime();
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
       date: new Date(2020, 6, 20, 10, 0, 0),
     });
 
@@ -33,9 +45,12 @@ describe('ListProviderDayAvailablity', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
