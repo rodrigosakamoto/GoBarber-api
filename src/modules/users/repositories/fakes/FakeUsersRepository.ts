31a17114234992683fc46a9cbebb80131dec219a
rodrigosakamoto <rodosakamoto@gmail.com>
2020-07-16 12:47:47 -0300

    feat: ListProviders

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,7 +1,9 @@
 import { uuid } from 'uuidv4';
 
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
+
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '../../infra/typeorm/entities/User';
 
@@ -18,6 +20,18 @@ class FakeUsersRepository implements IUserRepository {
     const findUser = this.users.find(user => user.email === email);
 
     return findUser;
+  }
+
+  public async findAllProviers({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
