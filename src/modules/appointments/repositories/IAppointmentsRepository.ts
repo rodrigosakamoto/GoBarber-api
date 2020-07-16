0f0efbf66b94ec2c88c0340a063157d653c59593
rodrigosakamoto <rodosakamoto@gmail.com>
2020-07-16 13:48:39 -0300

    feat: ListProviderMonthAvailability

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,8 +1,12 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 import ICrateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICrateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
